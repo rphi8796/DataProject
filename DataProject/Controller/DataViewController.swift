23c08112a7013104d36ebc76546abc1ad75f8679
rphi8796 <jlkfop@gmail.com>
2018-11-26 12:23:20 -0700

    I created the framework for this project

---
+++ DataProject/Controller/DataViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DataProject
+//
+//  Created by Phillips, Ryan on 11/26/18.
+//  Copyright © 2018 Phillips, Ryan. All rights reserved.
+//
+
+import UIKit
+
+public class DataViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    public override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
