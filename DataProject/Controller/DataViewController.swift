7b62164fe445f0036eacac7597971b5af222c83e
rphi8796 <jlkfop@gmail.com>
2018-11-30 13:14:23 -0700

    I finished it I think I just need to run it

--- DataProject/Controller/DataViewController.swift
+++ DataProject/Controller/DataViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class DataViewController: UITableViewController {
 
+    lazy var bucketList : [BucketItem] =
+    {
+        return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        do
+        {
+            if let filePath = Bundle.main.url(forResource: "bucket18", withExtension: "csv")
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+        }
+        catch
+        {
+            print("File load error")
+        }
+        
+        return items
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +53,25 @@ class DataViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return bucketList.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        cell.currentBucketItem = bucketList[indexPath.row]
+        
+        return cell
     }
 
     /*
